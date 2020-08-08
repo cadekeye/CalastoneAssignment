fc710ec4bb17c3bab43c5c95973d1ec90ed7dfef
cadekeye <53061131+cadekeye@users.noreply.github.com>
2020-08-08 18:22:03 +0100

    first commit

---
+++ CalastoneUnitTest/TextFileProcessorServiceTest.cs
@@ -0,0 +1,53 @@
+﻿using CalastoneAssignment;
+using CalastoneLibrary;
+using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
+
+namespace CalastoneUnitTest
+{
+    [TestFixture]
+    public class TextFileProcessorServiceTest
+    {
+        [TestCase("Alice was beginning to get very tired of sitting by her sister on the bank")]
+        public void TestGetContentAfterApplyFilterRulesAsync(string input)
+        {
+            IUtility _utility = new Utility();
+            ITextFileProcessorService _textFileProcessorService = new TextFileProcessorService(_utility);
+
+            var actual = _textFileProcessorService.GetContentAfterApplyFilterRulesAsync(input)
+                .GetAwaiter()
+                .GetResult();
+
+            Assert.AreEqual("beginning", actual);
+            Assert.IsNotNull(actual);
+
+        }
+
+        [TestCase("Alice was to get very tired of sitting by her sister on the bank")]
+        public void TestGetContentAfterApplyFilterRulesAsyncReturnEmpty(string input)
+        {
+            IUtility _utility = new Utility();
+            ITextFileProcessorService _textFileProcessorService = new TextFileProcessorService(_utility);
+
+            var actual = _textFileProcessorService.GetContentAfterApplyFilterRulesAsync(input)
+                .GetAwaiter()
+                .GetResult();
+
+            Assert.IsEmpty(actual);
+
+        }
+
+        [TestCase(null)]
+        public void TestGetContentAfterApplyFilterRulesAsyncWithNullInputRaisedException(string input)
+        {
+            IUtility _utility = new Utility();
+            ITextFileProcessorService _textFileProcessorService = new TextFileProcessorService(_utility);
+
+            Assert.Throws<NullReferenceException>(() => _textFileProcessorService.GetContentAfterApplyFilterRulesAsync(input)
+                .GetAwaiter()
+                .GetResult());
+
+        }
+    }
+}
---
+++ CalastoneUnitTest/UtilityTest.cs
@@ -0,0 +1,64 @@
+﻿using CalastoneLibrary;
+using NUnit.Framework;
+using System;
+
+
+namespace CalastoneUnitTest
+{
+    [TestFixture]
+    public class UtilityTest
+    {
+
+        [TestCase("abcdfg;'")]
+        public void TestRemoveNonAlphaCharacter(string input)
+        {
+            IUtility _utility = new Utility();
+
+            var actual = _utility.RemoveNonAlphaCharacter(input)
+                .GetAwaiter()
+                .GetResult();
+
+            Assert.AreEqual("abcdfg", actual);
+
+        }
+
+        [TestCase(";'")]
+        [TestCase(";.[]()'")]
+        [TestCase("")]
+        public void TestRemoveNonAlphaCharacterReturnEmpty(string input)
+        {
+            IUtility _utility = new Utility();
+
+            var actual = _utility.RemoveNonAlphaCharacter(input)
+                .GetAwaiter()
+                .GetResult();
+
+            Assert.AreEqual("", actual);
+            Assert.IsEmpty(actual);
+
+        }
+
+        [TestCase("c:\\caleb\\calastone.txt")]
+        public void TestReadTextFileContents(string filePath)
+        {
+            IUtility _utility = new Utility();
+
+            var actual = _utility.ReadTextFileContentsAsync(filePath)
+                .GetAwaiter()
+                .GetResult();
+
+            Assert.IsNotEmpty(actual);
+            Assert.IsTrue(actual.Contains("Alice"));
+
+        }
+
+        [TestCase("")]
+        public void TestReadTextFileContentsWithInvalidPathRaisedException(string filePath)
+        {
+            IUtility _utility = new Utility();
+
+            Assert.Throws<ArgumentException>(() => _utility.ReadTextFileContentsAsync(filePath).GetAwaiter().GetResult());
+  
+        }
+    }
+}
